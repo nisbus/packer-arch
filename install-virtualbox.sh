cc078779bddf53cd6b092715b85141686b29928a
nisbus <nisbus@gmail.com>
2013-12-08 22:53:07 +0000

    Still trying to get puppet installed

--- install-virtualbox.sh
+++ install-virtualbox.sh
@@ -9,7 +9,7 @@ TIMEZONE='UTC'
 
 CONFIG_SCRIPT='/usr/local/bin/arch-config.sh'
 ROOT_PARTITION="${DISK}1"
-TARGET_DIR='/mnt'
+TARGET_DIR='/var/log'
 
 echo "==> clearing partition table on ${DISK}"
 /usr/bin/sgdisk --zap ${DISK}
@@ -32,8 +32,10 @@ echo "==> mounting ${ROOT_PARTITION} to ${TARGET_DIR}"
 
 echo '==> bootstrapping the base installation'
 /usr/bin/pacstrap ${TARGET_DIR} base base-devel
+echo '==> updating the base installation'
 /usr/bin/arch-chroot ${TARGET_DIR} pacman -Syu --noconfirm
-/usr/bin/arch-chroot ${TARGET_DIR} pacman -S --noconfirm gptfdisk openssh syslinux wget git expac jshon ruby
+echo '==> installing basics'
+/usr/bin/arch-chroot ${TARGET_DIR} pacman -S --noconfirm linux-headers gptfdisk openssh syslinux wget git expac jshon ruby mlocate
 /usr/bin/arch-chroot ${TARGET_DIR} syslinux-install_update -i -a -m
 /usr/bin/sed -i 's/sda3/sda1/' "${TARGET_DIR}/boot/syslinux/syslinux.cfg"
 /usr/bin/sed -i 's/TIMEOUT 50/TIMEOUT 10/' "${TARGET_DIR}/boot/syslinux/syslinux.cfg"
@@ -44,56 +46,67 @@ echo '==> generating the filesystem table'
 echo '==> generating the system configuration script'
 /usr/bin/install --mode=0755 /dev/null "${TARGET_DIR}${CONFIG_SCRIPT}"
 
+echo "==================> Installing packer and puppet"
+/usr/bin/mkdir packer && cd packer && /usr/bin/curl --output ./PKGBUILD https://aur.archlinux.org/packages/pa/packer/PKGBUILD && /usr/bin/makepkg && /usr/bin/pacman -U packer-*.pkg.tar.xz --noconfirm && cd .. >> /var/log/install.log
+/usr/bin/packer -S puppet --noconfirm >> /var/log/install.log
+echo "==================> Packer installed"
+
+echo "==================> Installing VirtualBox Guest Additions"
+/usr/bin/packer -S vboxguest-hook --noconfirm >> /var/log/install.log
+/usr/bin/pacman -S --noconfirm virtualbox-guest-utils virtualbox-guest-dkms >> /var/log/install.log
+echo -e 'vboxguest\nvboxsf\nvboxvideo' > /etc/modules-load.d/virtualbox.conf
+/usr/bin/systemctl enable dkms
+/usr/bin/systemctl enable vboxservice
+
 cat <<-EOF > "${TARGET_DIR}${CONFIG_SCRIPT}"
 	echo '${FQDN}' > /etc/hostname
 	/usr/bin/ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 	echo 'KEYMAP=${KEYMAP}' > /etc/vconsole.conf
 	/usr/bin/sed -i 's/#${LANGUAGE}/${LANGUAGE}/' /etc/locale.gen
+	
 	/usr/bin/locale-gen
-	/usr/bin/mkinitcpio -p linux
+	/usr/bin/mkinitcpio -A vboxguest -p linux >> /var/log/install.log
 	/usr/bin/usermod --password ${PASSWORD} root
 	# https://wiki.archlinux.org/index.php/Network_Configuration#Device_names
 	/usr/bin/ln -s /dev/null /etc/udev/rules.d/80-net-name-slot.rules
 	/usr/bin/ln -s '/usr/lib/systemd/system/dhcpcd@.service' '/etc/systemd/system/multi-user.target.wants/dhcpcd@eth0.service'
 	/usr/bin/sed -i 's/#UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
 	/usr/bin/systemctl enable sshd.service
-
-	# VirtualBox Guest Additions
-	/usr/bin/pacman -S --noconfirm linux-headers virtualbox-guest-utils virtualbox-guest-dkms
-	echo -e 'vboxguest\nvboxsf\nvboxvideo' > /etc/modules-load.d/virtualbox.conf
-	guest_version=\$(/usr/bin/pacman -Q virtualbox-guest-dkms | awk '{ print \$2 }' | cut -d'-' -f1)
-	kernel_version="\$(/usr/bin/pacman -Q linux | awk '{ print \$2 }')-ARCH"
-	/usr/bin/dkms install "vboxguest/\${guest_version}" -k "\${kernel_version}/x86_64"
-	/usr/bin/systemctl enable dkms.service
-	/usr/bin/systemctl enable vboxservice.service
-
+		
+	echo "======================>   Setting up network....................................."	
+	# enable dhcpcd
+	/usr/bin/systemctl enable dhcpcd >> /var/log/install.log
+		
+	echo "======================>   Setting up vagrant user....................................."		
 	# Vagrant-specific configuration
 	/usr/bin/groupadd vagrant
 	/usr/bin/useradd --password ${PASSWORD} --comment 'Vagrant User' --create-home --gid users --groups vagrant,vboxsf vagrant
 	echo 'Defaults env_keep += "SSH_AUTH_SOCK"' > /etc/sudoers.d/10_vagrant
 	echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers.d/10_vagrant
-	/usr/bin/chmod 0440 /etc/sudoers.d/10_vagrant
+	/usr/bin/chmod 0440 /etc/sudoers.d/10_vagrant >> /var/log/install.log
 	/usr/bin/install --directory --owner=vagrant --group=users --mode=0700 /home/vagrant/.ssh
 	/usr/bin/curl --output /home/vagrant/.ssh/authorized_keys https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub
 	/usr/bin/chown vagrant:users /home/vagrant/.ssh/authorized_keys
 	/usr/bin/chmod 0600 /home/vagrant/.ssh/authorized_keys
+	
 	# workaround for shutdown race condition: http://comments.gmane.org/gmane.linux.arch.general/48739
-	/usr/bin/curl --output /etc/systemd/system/poweroff.timer https://raw.github.com/elasticdog/packer-arch/master/poweroff.timer
+	/usr/bin/curl --output /etc/systemd/system/poweroff.timer https://raw.github.com/nisbus/packer-arch/master/poweroff.timer
 
+	echo "======================>   Cleaning up....................................."			
 	# clean up
 	/usr/bin/pacman -Rcns --noconfirm gptfdisk
-	/usr/bin/pacman -Scc --noconfirm
+	
+	#This is considered bad practice
+	#/usr/bin/pacman -Scc --noconfirm
+	
+	/usr/bin/sleep 5
 EOF
 
 echo '==> entering chroot and configuring system'
-/usr/bin/arch-chroot ${TARGET_DIR} wget https://aur.archlinux.org/packages/pa/packer/packer.tar.gz
-/usr/bin/arch-chroot ${TARGET_DIR} tar -xvf https://aur.archlinux.org/packages/pa/packer/packer.tar.gz && cd packer && mkpkg && pacman -U packer-*.pkg.tar.xz --noconfirm
-/usr/bin/arch-chroot ${TARGET_DIR} packer -S puppet --noconfirm
-/usr/bin/arch-chroot ${TARGET_DIR} systemctl enable dhcpcd
 /usr/bin/arch-chroot ${TARGET_DIR} ${CONFIG_SCRIPT}
-rm "${TARGET_DIR}${CONFIG_SCRIPT}"
+#rm "${TARGET_DIR}${CONFIG_SCRIPT}"
 
-echo '==> installation complete!'
+echo '==> installation complete!...........................................'
 /usr/bin/sleep 3
 /usr/bin/umount ${TARGET_DIR}
-/usr/bin/systemctl reboot
+/usr/bin/systemctl reboot -f
